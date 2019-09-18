662b635b22eb2420e1bd7c2d851e65e3e5ca252a
740291272 <740291272@qq.com>
2019-09-18 13:33:23 +0800

    增加字库与测试字体显示

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,18 +1,8 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #include "MainComponent.h"
 
 //==============================================================================
 MainComponent::MainComponent() {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize(800, 600);
+    setSize(1024, 576);
 
     // Some platforms require permissions to open input channels so request that here
     if (RuntimePermissions::isRequired(RuntimePermissions::recordAudio) && !RuntimePermissions::isGranted(RuntimePermissions::recordAudio)) {
@@ -24,47 +14,33 @@ MainComponent::MainComponent() {
 }
 
 MainComponent::~MainComponent() {
-    // This shuts down the audio device and clears the audio source.
     shutdownAudio();
 }
 
 //==============================================================================
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate) {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+    
 }
 
 void MainComponent::getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill) {
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
     bufferToFill.clearActiveBufferRegion();
 }
 
 void MainComponent::releaseResources() {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
+    
 }
 
 //==============================================================================
 void MainComponent::paint(Graphics &g) {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
-
-    // You can add your drawing code here!
+    g.setColour(Colours::white);
+    g.drawText(CharPointer_UTF8("\xe4\xbd\xa0\xe5\xa5\xbd\xef\xbc\x81"
+                                "\xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf\xef\xbc\x81"
+                                "Hello!"),
+               getLocalBounds(), Justification::centred);
 }
 
-void MainComponent::resized() {
+void MainComponent::resized(){
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
