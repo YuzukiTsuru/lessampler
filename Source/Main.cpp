8e19c8bf695b4ebc128aabbaa50f6fb94c2f5f64
740291272 <740291272@qq.com>
2019-09-20 10:45:47 +0800

    add to git
    Former-commit-id: 93f71fd27e1cd628edf0ee4795aa03a677486aa5

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,72 +1,34 @@
+#include "Main.h"
+
+#include <GLFW/glfw3.h>
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "MainComponent.h"
 
-class LESSAMPLERApplication : public JUCEApplication {
-  public:
-    LESSAMPLERApplication() {}
 
-    const String getApplicationName() override { return ProjectInfo::projectName; }
-    const String getApplicationVersion() override { return ProjectInfo::versionString; }
-    bool moreThanOneInstanceAllowed() override { return true; }
-    void initialise(const String &commandLine) override {
-        Typeface::Ptr getTypefaceForFont(const Font &f);
-        LookAndFeel::setDefaultLookAndFeel(&customLookAndFeel);
-        mainWindow.reset(new MainWindow(getApplicationName()));
+int main(int argc, char *argv[]) {
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
     }
 
-    void shutdown() override {
-        mainWindow = nullptr;
-    }
-    void systemRequestedQuit() override {
-        quit();
-    }
-
-    void anotherInstanceStarted(const String &commandLine) override {
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(320, 180, "LESSAMPLER", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
     }
 
-    class MainWindow : public DocumentWindow {
-      public:
-        MainWindow(String name) : DocumentWindow(name, Desktop::getInstance().getDefaultLookAndFeel().findColour(ResizableWindow::backgroundColourId), DocumentWindow::allButtons) {
-            setUsingNativeTitleBar(true);
-            setContentOwned(new MainComponent(), true);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-#if JUCE_IOS || JUCE_ANDROID
-            setFullScreen(true);
-#else
-            setResizable(true, true);
-            centreWithSize(getWidth(), getHeight());
-#endif
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-            setVisible(true);
-        }
-
-        void closeButtonPressed() override {
-            JUCEApplication::getInstance()->systemRequestedQuit();
-        }
-
-      private:
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainWindow)
-    };
-
-  private:
-    class CustomFontLookAndFeel : public LookAndFeel_V4 {
-      public:
-        CustomFontLookAndFeel() {
-            LookAndFeel::setDefaultLookAndFeel(this);
-        }
-
-        static const Font getCustomFont() {
-            static auto typeface = Typeface::createSystemTypefaceFor(BinaryData::SiHei_otf, BinaryData::SiHei_otfSize);
-            return Font(typeface);
-        }
-
-        Typeface::Ptr getTypefaceForFont(const Font &f) override {
-            return getCustomFont().getTypeface();
-        }
-
-      private:
-    } customLookAndFeel;
-    std::unique_ptr<MainWindow> mainWindow;
-};
-
-START_JUCE_APPLICATION(LESSAMPLERApplication)
+    glfwTerminate();
+    return 0;
+}
