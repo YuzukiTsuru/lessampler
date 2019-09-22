88dc82d2b8d7d195c9129624c1a4eed8683c6627
740291272 <740291272@qq.com>
2019-09-22 13:24:19 +0800

    add dialog
    Former-commit-id: 06a6accffbf35bba00e01911c954fbfcbd696a87

---
+++ Source/Dialogs.cpp
@@ -0,0 +1,36 @@
+#include "Dialogs.h"
+
+#include <OpenGL/gl3.h>
+#include <GLFW/glfw3.h>
+
+int errorDialog() {
+    GLFWwindow *window;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
--- Source/Main.h
+++ Source/Main.h
@@ -1,7 +1,2 @@
 #pragma once
 
-#ifdef __APPLE__
-# define __gl_h_
-# define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
-#endif
-
