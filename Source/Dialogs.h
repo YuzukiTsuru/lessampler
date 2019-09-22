7a0487ab62769ccd5afa3e1df9159fe9623bc80d
740291272 <740291272@qq.com>
2019-09-22 14:41:33 +0800

    add a dialog

--- Source/Dialogs.h
+++ Source/Dialogs.h
@@ -6,8 +6,23 @@
 #define DEFAULT_PATH "/tmp"
 #endif
 
-// Error Dialog Display
-int errorDialog();
+/*
+ * Error Dialog Display
+ *
+ * IN: Error Messages
+ *
+ * OUT: int
+ */
+int errorDialog(const char *error);
+
+/*
+ * Info Dialog Display
+ *
+ * IN: Title Info
+ *
+ * OUT: void
+ */
+void infoDialog(const char *title, const char *msg);
 
 // CopyRight
 void copyRight();
