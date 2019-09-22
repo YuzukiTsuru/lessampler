9e807f83d5e8c923b8af0055ce9ae6bd353354a5
740291272 <740291272@qq.com>
2019-09-22 14:23:57 +0800

    更新了UI接口

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,11 +1,11 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 
 #include "Dialogs.h"
-
 #include "Main.h"
 
 int main(int argc, char *argv[]) {
     if(argc < 3){
+        copyRight();
         errorDialog();
     }
     return 0;
