06a6accffbf35bba00e01911c954fbfcbd696a87
740291272 <740291272@qq.com>
2019-09-22 13:24:19 +0800

    add dialog

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,34 +1,12 @@
-#include "Main.h"
-
-#include <GLFW/glfw3.h>
 #include "../JuceLibraryCode/JuceHeader.h"
 
+#include "Dialogs.h"
+
+#include "Main.h"
 
 int main(int argc, char *argv[]) {
-    GLFWwindow *window;
-
-    /* Initialize the library */
-    if (!glfwInit()) {
-        return -1;
+    if(argc < 3){
+        errorDialog();
     }
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(320, 180, "LESSAMPLER", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return 0;
 }
