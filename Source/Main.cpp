d1869b1f91d9cdbbcd5778ff198939bdd2b0fd7f
740291272 <740291272@qq.com>
2019-09-29 22:57:06 +0800

    update for pull

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,11 +1,13 @@
 #include "Main.h"
 
 int main(int argc, char *argv[]) {
-    if(argc < 3){
+    if (argc < 3) {
         dialog.copyRight();
         int feedBack = dialog.errorDialog("The input parameter is wrong.\nplease check if it is called correctly or the parameter is correct.");
-        if(feedBack == 1){
+        if (feedBack == 1) {
             dialog.infoDialog("Use Suggestions", "Please call in the UTAU program or call with relevant parameters");
+        } else if (feedBack == -1) {
+            dialog.infoDialog("Whats Wrong？？？", "Whats Wrong？？？\n Should not be like this......");
         }
     }
     return 0;
