cc70f532728864a4bda5a7cbf9a5750be3812d55
740291272 <740291272@qq.com>
2019-09-30 12:24:49 +0800

    你好了吗？我好了

--- Source/BasicFile.h
+++ Source/BasicFile.h
@@ -8,8 +8,8 @@ std::string initLangStr = {
     "\t\t<1>The input parameter is wrong.please check if it is called correctly or the parameter is correct.</1>\n"
     "\t\t<2>Use Suggestions</2>\n"
     "\t\t<3>Please call in the UTAU program or call with relevant parameters</3>\n"
-    "\t\t<4>Whats Wrong？？？</4>\n"
-    "\t\t<5>Whats Wrong？？ Should not be like this......</5>\n"
+    "\t\t<4>Whats Wrong???</4>\n"
+    "\t\t<5>Whats Wrong??? Should not be like this......</5>\n"
     "\t</en_US>\n"
     "</LESSAMPLERLANG>"
 };
