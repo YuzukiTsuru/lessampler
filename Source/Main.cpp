b0d8cf48b1502fa3fb703ab0457a9e9ef9e3d60b
740291272 <740291272@qq.com>
2019-09-30 17:02:59 +0800

    i18n Ready!

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
     // Find the xml file for it
-    if (!langpack.exists("config.json") || !langpack.exists("lang.json")) {
+    if (!langpack.exists("config.json") || !langpack.exists("en_US.json")) {
         int feedback = dialog.errorDialog("Missing configuration file,Do you want to create a file?\n\n缺失配置文件，是否新建文件？\n\n構成ファイルがありません，ファイルを作成しますか？");
         if (feedback == 1) {
             langpack.initFile();
@@ -15,13 +15,14 @@ int main(int argc, char *argv[]) {
 
     if (argc < 3) {
         dialog.copyRight();
-        int feedBack = dialog.errorDialog("The input parameter is wrong.please check if it is called correctly or the parameter is correct.");
+        int feedBack = dialog.errorDialog(langpack.tr("Str1")); // The input parameter is wrong. please check if it is called correctly or the parameter is correct.
         if (feedBack == 1) {
-            dialog.infoDialog("Use Suggestions", "Please call in the UTAU program or call with relevant parameters");
+            // Use Suggestions | Please call in the UTAU program or call with relevant parameters
+            dialog.infoDialog(langpack.tr("Str2"), langpack.tr("Str3"));
         } else if (feedBack == -1) {
-            dialog.infoDialog("Whats Wrong???", "Whats Wrong??? Should not be like this......");
+            // Whats Wrong??? | Whats Wrong??? Should not be like this......
+            dialog.infoDialog(langpack.tr("Str4"), langpack.tr("Str5"));
         }
     }
-    std::cout << langpack.fetch("1");
     return 0;
 }
