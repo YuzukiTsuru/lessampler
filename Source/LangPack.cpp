133396482e244f29f1fb072480f80c1b9235a05a
740291272 <740291272@qq.com>
2019-09-30 08:56:16 +0800

    clean and orgize

--- Source/LangPack.cpp
+++ Source/LangPack.cpp
@@ -1,20 +1,42 @@
 #include <locale.h>
 #include <sys/stat.h>
+#include <fstream>
+#include "BasicFile.h"
 #include "LangPack.h"
 
 LangPack::LangPack() {}
 
-void LangPack::fetch(char *instr) {
-    doc.LoadFile("lang.xml");
+void LangPack::fetch(char *filename, char *instr) {
+    doc.LoadFile(filename);
 }
 
 const char *LangPack::systemLanguage() {
     doc.LoadFile("config.xml");
-    const char* langName = doc.FirstChildElement("LESSAMPLERSETTING")->FirstChildElement("LANGUAGE")->GetText();
+    const char *langName = doc.FirstChildElement("LESSAMPLERSETTING")->FirstChildElement("LANGUAGE")->GetText();
     return langName;
 }
 
-bool LangPack::exists (const char *name) {
-  struct stat buffer;
-  return (stat (name, &buffer) == 0);
+bool LangPack::exists(const char *name) {
+    struct stat buffer;
+    return stat(name, &buffer) == 0;
+}
+
+void LangPack::initFile() {
+    // Open Stream
+    std::ofstream File;
+    // Open file
+    File.open("config.xml");
+    if (File.is_open()) {
+        File << initConfigFileStr;
+    }else{
+        dialog.errorDialog("No write permission");
+    }
+    File.close();
+    File.open("lang.xml");
+    if (File.is_open()) {
+        File << initLangStr;
+    }else{
+        dialog.errorDialog("No write permission");
+    }
+    File.close();
 }
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,19 +1,25 @@
 #include "Main.h"
 
 int main(int argc, char *argv[]) {
-    //
-    if(!langpack.exists("config.xml") || !langpack.exists("language.xml" )){
-        dialog.errorDialog("Missing configuration file\n缺失配置文件\n構成ファイルがありません");
-        return -1;
+    // Find the xml file for it
+    if (!langpack.exists("config.xml") || !langpack.exists("lang.xml")) {
+        int feedback = dialog.errorDialog("Missing configuration file,Do you want to create a file?\n缺失配置文件，是否新建文件？\n構成ファイルがありません，ファイルを作成しますか？");
+        if (feedback == 1) {
+            langpack.initFile();
+        } else if (feedback == 2) {
+            return -1;
+        } else {
+            return -1;
+        }
     }
-    
+
     if (argc < 3) {
         dialog.copyRight();
-        int feedBack = dialog.errorDialog("The input parameter is wrong.\nplease check if it is called correctly or the parameter is correct.");
+        int feedBack = dialog.errorDialog("The input parameter is wrong.please check if it is called correctly or the parameter is correct.");
         if (feedBack == 1) {
             dialog.infoDialog("Use Suggestions", "Please call in the UTAU program or call with relevant parameters");
         } else if (feedBack == -1) {
-            dialog.infoDialog("Whats Wrong？？？", "Whats Wrong？？？\n Should not be like this......");
+            dialog.infoDialog("Whats Wrong？？？", "Whats Wrong？？？Should not be like this......");
         }
     }
     return 0;
