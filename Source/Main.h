f833d2c87236fb92cbcacbeea64a95202a22caee
GloomyGhost <740291272@qq.com>
2019-12-26 14:25:52 +0800

    fix logic error

--- Source/Main.h
+++ Source/Main.h
@@ -1,42 +1,41 @@
 #pragma once
 
-#include "Dialogs.h"
-#include "LangPack.h"
 #include "../JuceLibraryCode/JuceHeader.h"
-
+#include "Dialogs.h"
+#include "FileExists.h"
+#include "LangPack.h"
 
 typedef struct {
-    int offset_ms;
-    int cutoff_ms;
-    double *f0;
-    double *FixedF0;
-    double *TimeAxis;
-    double *y;
-    double *x;
-    double **Specgram;
-    int NBit;
-    int *SpecgramLength;
-    int *SpecgramIndex;
-    int *FixedSpecgramIndex;
-    int PCount;
-    double FramePeriod;
-    int x_length;
-    int y_length;
-    int f0_length;
-    int f0_length2;
-    double GRatio;
-    double *Volume;
-    double *FixedVolume;
-    int fs;
-    double Velocity;
-    double VRatio;
-    int LengthMsec;
-    int Sf0LengthMsec;
-    int InputF0LengthMsec;
-    double f0_floor;
-    int DefaultF0;
+  int offset_ms;
+  int cutoff_ms;
+  double* f0;
+  double* FixedF0;
+  double* TimeAxis;
+  double* y;
+  double* x;
+  double** Specgram;
+  int NBit;
+  int* SpecgramLength;
+  int* SpecgramIndex;
+  int* FixedSpecgramIndex;
+  int PCount;
+  double FramePeriod;
+  int x_length;
+  int y_length;
+  int f0_length;
+  int f0_length2;
+  double GRatio;
+  double* Volume;
+  double* FixedVolume;
+  int fs;
+  double Velocity;
+  double VRatio;
+  int LengthMsec;
+  int Sf0LengthMsec;
+  int InputF0LengthMsec;
+  double f0_floor;
+  int DefaultF0;
 } LESS;
-
 
 /*
  * init Dialogs Object
@@ -44,7 +43,11 @@ typedef struct {
 Dialogs dialog;
 
 /*
+ * Init File Seeting
+ */
+FileExists filexists;
+
+/*
  * Init Language Pack
  */
 LangPack langpack;
-
