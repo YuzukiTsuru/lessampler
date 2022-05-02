9ac594a3cb01de00c43829c83fb905b5aaf3c84b
YuzukiTsuru <740291272@qq.com>
2022-05-02 22:03:07 +0800

    merge FileIO to new lessampler

--- src/FileIO/FileBasic.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Ghost Gloomy on 2020/8/27.
-//
-
-#include "FileBasic.h"
-
-std::string less::FileBasic::MakeFileName(const std::string &filename, const std::string &ext) {
-    return filename.substr(0, filename.find_last_of(".")) + ext;
-}
