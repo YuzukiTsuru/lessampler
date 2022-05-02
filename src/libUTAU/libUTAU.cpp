c635b4cb3c968e74a09022e8047a6de70ff496ca
YuzukiTsuru <740291272@qq.com>
2022-05-02 13:01:22 +0800

    merge libUTAU to lessampler
    Former-commit-id: 2adaf354ce2b3c0c8c1e3bee1bca7a9099756c3a

---
+++ src/libUTAU/libUTAU.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c)  2022, YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include "UTAUException.h"
+#include "UTAUParameterPaser.h"
+
+#include "libUTAU.h"
+
+libUTAU::libUTAU(int argc, char **argv) {
+    if (argc < 3) {
+        throw parameter_error("too sort");
+    }
+    UTAUParameterPaser paser(argc, argv);
+    utauPara = paser.get_para();
+}
+
+void libUTAU::printUTAUPara() const {
+    std::cout << "Program Name:           " << utauPara.localName << std::endl;
+    std::cout << "In File Name:           " << utauPara.inputFileName << std::endl;
+    std::cout << "Out File Name:          " << utauPara.outputFileName << std::endl;
+    std::cout << "Scale Name:             " << utauPara.scaleName << std::endl;
+    std::cout << "Scale Number:           " << utauPara.scaleNum << std::endl;
+    std::cout << "Time Percent:           " << utauPara.timePercent << std::endl;
+    std::cout << "Velocity:               " << utauPara.velocity << std::endl;
+    std::cout << "Flags:                  " << utauPara.flags << std::endl;
+    std::cout << "Offset:                 " << utauPara.offset << std::endl;
+    std::cout << "Host Required Length:   " << utauPara.requiredLength << std::endl;
+    std::cout << "First Half Fixed Part:  " << utauPara.firstHalfFixedPart << std::endl;
+    std::cout << "Last Unused Part:       " << utauPara.lastUnusedPart << std::endl;
+    std::cout << "Volumes:                " << utauPara.volumes << std::endl;
+    std::cout << "Tempo:                  " << utauPara.tempo << std::endl;
+    std::cout << "Tempo Number:           " << utauPara.tempoNum << std::endl;
+    std::cout << "Pitch String:           " << utauPara.pitch << std::endl;
+}
+
+UTAUPara libUTAU::getUTAUPara() const {
+    return utauPara;
+}
---
+++ src/libUTAU/libUTAU.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c)  2022, YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LESSAMPLER_LIBUTAU_H
+#define LESSAMPLER_LIBUTAU_H
+
+#include <iostream>
+
+class UTAUPara {
+public:
+    std::string localName = {};
+    std::string inputFileName = {};
+    std::string outputFileName = {};
+    std::string scaleName = {};
+    double scaleNum = 0.0;
+    int timePercent = 0;
+    double velocity = 0.0;
+    std::string flags = {};
+    double offset = 0.0;
+    double requiredLength = 0.0;
+    double firstHalfFixedPart = 0.0;
+    double lastUnusedPart = 0.0;
+    double volumes = 0;
+    int modulation = 0;
+    std::string tempo = {};
+    int tempoNum = 0;
+    std::string pitch = {};
+};
+
+class libUTAU {
+public:
+    libUTAU(int argc, char *argv[]);
+
+    void printUTAUPara() const;
+
+    UTAUPara getUTAUPara() const;
+
+private:
+    UTAUPara utauPara;
+};
+
+#endif //LESSAMPLER_LIBUTAU_H
