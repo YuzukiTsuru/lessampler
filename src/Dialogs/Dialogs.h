b8b843197a4d5b7dd8f00c3f9b13ec6e30377827
YuzukiTsuru <740291272@qq.com>
2022-05-02 14:17:47 +0800

    add basic dialog for modeling the sound set
    Former-commit-id: 3b1c9ffb42a8ae32b12ff42b182ad63e29467dd8

--- src/Dialogs/Dialogs.h
+++ src/Dialogs/Dialogs.h
@@ -14,15 +14,11 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-//
-// Created by gloom on 2022/5/2.
-//
-
 #ifndef LESSAMPLER_DIALOGS_H
 #define LESSAMPLER_DIALOGS_H
 
-
 class Dialogs {
+    Dialogs();
 
 };
 
