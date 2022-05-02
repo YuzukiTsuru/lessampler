cea4f5a1e9a966ed0f39cd5a255356cce4218c19
YuzukiTsuru <740291272@qq.com>
2022-05-02 10:03:04 +0800

    add configure file and update log name
    Former-commit-id: 81bd64bbb29b0c9b24560491fe99173b8757d1f6

---
+++ src/AudioModel/libWorldWrapper.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c)  2022, YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with libllsm. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// Created by gloom on 2022/5/2.
+//
+
+#ifndef LESSAMPLER_LIBWORLDWRAPPER_H
+#define LESSAMPLER_LIBWORLDWRAPPER_H
+
+
+class libWorldWrapper {
+};
+
+
+#endif //LESSAMPLER_LIBWORLDWRAPPER_H
