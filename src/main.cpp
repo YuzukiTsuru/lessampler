bf0fc894146f565997ca689cff6b5bded35f795d
YuzukiTsuru <740291272@qq.com>
2022-05-02 13:13:29 +0800

    add lessampler class for internal use

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]){
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <number>" << std::endl;
+        return 1;
+    }
     return 0;
 }
