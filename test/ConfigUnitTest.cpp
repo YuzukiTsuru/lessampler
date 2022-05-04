48c768259195a2ec0569349d04910a10df0ea763
YuzukiTsuru <740291272@qq.com>
2022-05-04 14:08:05 +0800

    test: if found config file, delete
    Former-commit-id: c5c63a43c55cc3bdf6f5ee7cc2b6aecb9834720c

--- test/ConfigUnitTest.cpp
+++ test/ConfigUnitTest.cpp
@@ -2,9 +2,16 @@
 // Created by gloom on 2022/5/4.
 //
 
+#include <filesystem>
 #include "ConfigUnit.h"
 
 int main(int argc, char *argv[]) {
-    ConfigUnit configUnit("config.ini");
+    if (std::filesystem::exists("config.ini")) {
+        std::filesystem::remove("config.ini");
+    }
+    // Create a config file
+    ConfigUnit configUnit_1("config.ini");
+    // Parse the config file
+    ConfigUnit configUnit_2("config.ini");
     return 0;
 }
