c54764acb8efc6533bb0e8c3dfc5b06ee777e345
YuzukiTsuru <740291272@qq.com>
2022-05-23 15:57:21 +0800

    add basic audio model generator
    Former-commit-id: dafa324415c65f87d07dd15e3edea88e3d968a04

---
+++ src/GenerateAudioModel.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2022. YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+//
+// Created by gloom on 2022/5/23.
+//
+
+#include <Utils/LOG.h>
+
+#include <utility>
+
+#include "GenerateAudioModel.h"
+
+GenerateAudioModel::GenerateAudioModel(std::filesystem::path path) : target_voice_path(std::move(path)) {
+    YALL_INFO_ << "Get the audio files in the directory";
+    GetWavFileLists();
+#ifdef DEBUG_MODE
+    PrintWavFiles();
+#endif
+}
+
+void GenerateAudioModel::PrintWavFiles() {
+    for (const auto &file: wav_files) {
+        YALL_DEBUG_ << file.string() + "\n";
+    }
+}
+
+void GenerateAudioModel::GetWavFileLists() {
+    YALL_INFO_ << "Working on folder: " + target_voice_path.string();
+    for (const auto &entry: std::filesystem::directory_iterator(target_voice_path)) {
+        if (entry.path().extension() == ".wav") {
+            wav_files.push_back(entry.path());
+        }
+    }
+}
+
