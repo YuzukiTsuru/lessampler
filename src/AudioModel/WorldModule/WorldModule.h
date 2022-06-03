479f38962f87021ff7f6ef611795ac5fe7b87a30
YuzukiTsuru <740291272@qq.com>
2022-06-03 12:16:09 +0800

    yolo push

---
+++ src/AudioModel/WorldModule/WorldModule.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c)  2022, YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// Created by gloom on 2022/5/2.
+//
+
+#ifndef LESSAMPLER_WORLDMODULE_H
+#define LESSAMPLER_WORLDMODULE_H
+
+#include "WorldPara.h"
+#include "ConfigUnit/lessConfigure.h"
+
+class WorldModule {
+public:
+    WorldModule(double *x, int x_length, int fs, const lessConfigure &configure);
+
+    ~WorldModule();
+
+    WorldPara GetModule();
+
+private:
+    void F0EstimationDio();
+
+    void F0EstimationHarvest();
+
+    void SpectralEnvelopeEstimation();
+
+    void AperiodicityEstimation();
+
+protected:
+    double *x = nullptr;
+    int x_length = 0;
+
+    WorldPara worldPara;
+
+    lessConfigure configure;
+};
+
+#endif //LESSAMPLER_WORLDMODULE_H
---
+++ src/Shine/Binding/libUTAU/ScaleConvert.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c)  2022, YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LESSAMPLER_SCALECONVERT_H
+#define LESSAMPLER_SCALECONVERT_H
+
+#include <cmath>
+#include <iostream>
+
+class ScaleConvert {
+public:
+    explicit ScaleConvert(std::string scaleName);
+
+    [[nodiscard]] double GetScaleNum() const;
+
+private:
+    double scaleNum = 0.0;
+    /*
+     * Scale Name Convert to number than double
+     * IN: std::string scale_name
+     * RET: bool
+     *   true -> success
+     *   false -> fail
+     * OUT: double scale_num
+     */
+    bool ScaleConvertToDouble(std::string scaleName);
+};
+
+
+#endif //LESSAMPLER_SCALECONVERT_H
