6eb5de1014ac6ccc44785fd00fe7f3e384e9c862
YuzukiTsuru <740291272@qq.com>
2022-07-03 14:02:58 +0800

    update suit for visual studio mvcc

--- test/VecbinTest.cpp
+++ test/VecbinTest.cpp
@@ -2,44 +2,58 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cmath>
+
+#define _PI                      3.14159265358979               // PI
+#define _HALF_PI                 1.57079632679490               // PI / 2
+#define _QUARTER_PI              0.78539816339745               // PI / 4
+#define _TWAIN_PI                6.28318530717958               // PI * 2
+#define _E                       2.71828182845905               // e
+#define _LOG2E                   1.44269504088896               // log2(e)
+#define _LOG10E                  0.43429448190325               // log10(e)
+#define _LN2                     0.69314718055995               // ln(2)
+#define _LN10                    2.30258509299405               // ln(10)
+#define _SQRT_2                  1.41421356237310               // sqrt(2)
+#define _SQRT_1_2                0.70710678118655               // 1/sqrt(2)
+
 
 int main() {
     // 测试数据
     std::string String = "Strings";
-    std::vector<double> Vector1D(10, M_1_PI);
-    std::vector<std::vector<double>> Vector2D{{M_E,   M_LN10},
-                                              {M_LN2, M_LOG2E},
-                                              {M_E,   M_LN10},
-                                              {M_LN2, M_LOG2E},
-                                              {M_LN2, M_LOG2E},
-                                              {M_E,   M_LN10},
-                                              {M_LN2, M_LOG2E},
-                                              {M_LN2, M_LOG2E}};
+    std::vector<double> Vector1D(10, _PI);
+    std::vector<std::vector<double>> Vector2D{
+            {_HALF_PI,  _QUARTER_PI},
+            {_TWAIN_PI, _E},
+            {_LOG2E,    _LOG10E},
+            {_LN2,      _LN10},
+            {_SQRT_2,   _SQRT_1_2},
+            {_LOG2E,    _LOG10E},
+            {_LN2,      _LN10},
+            {_SQRT_2,   _SQRT_1_2}
+    };
 
     std::ofstream ofstream("test.bin", std::ios::out | std::ios::binary);
 
     // 首先需要写入长度，以 char 格式保存。但是由于 String.size 是一个临时变量无法写入，所以需要赋值新变量
-    uint32_t outStringSize = String.size();
+    std::streamsize outStringSize = String.size();
     // 将指向类型的指针转换为 <char *> 并给出类型的 sizeof 写入
-    ofstream.write(reinterpret_cast<const char *>(&outStringSize), sizeof(uint32_t));
+    ofstream.write(reinterpret_cast<const char *>(&outStringSize), sizeof(std::streamsize));
     ofstream.write(String.c_str(), std::streamsize(outStringSize * sizeof(char)));
 
     // 写入一维 Vector
-    uint32_t outVector1DSize = Vector1D.size();
+    std::streamsize outVector1DSize = Vector1D.size();
     // 使用了 Vector 的特性，&vector<T>[0] 是 T 类型的指针
     // 将指向类型的指针转换为 <char *> 并给出 <长度 * sizeof<T>>
-    ofstream.write(reinterpret_cast<const char *>(&outVector1DSize), sizeof(uint32_t));
-    ofstream.write(reinterpret_cast<const char *>(&Vector1D[0]), std::streamsize(Vector1D.size() * sizeof(double)));
+    ofstream.write(reinterpret_cast<const char *>(&outVector1DSize), sizeof(std::streamsize));
+    ofstream.write(reinterpret_cast<const char *>(&Vector1D[0]), Vector1D.size() * sizeof(double));
 
     // 写入二维 Vector
     // 二维 Vector 不能直接写入，需要一层遍历
-    uint32_t outVector2DSize = Vector2D.size();
-    ofstream.write(reinterpret_cast<const char *>(&outVector2DSize), sizeof(uint32_t));
+    std::streamsize outVector2DSize = Vector2D.size();
+    ofstream.write(reinterpret_cast<const char *>(&outVector2DSize), sizeof(std::streamsize));
     for (auto &item: Vector2D) {
-        uint32_t size = item.size();
-        ofstream.write(reinterpret_cast<const char *>(&size), sizeof(uint32_t));
-        ofstream.write(reinterpret_cast<const char *>(&item[0]), std::streamsize(item.size() * sizeof(double)));
+        std::streamsize size = item.size();
+        ofstream.write(reinterpret_cast<const char *>(&size), sizeof(std::streamsize));
+        ofstream.write(reinterpret_cast<const char *>(&item[0]), item.size() * sizeof(double));
     }
 
     ofstream.close();
@@ -48,8 +62,8 @@ int main() {
     std::ifstream ifstream("test.bin", std::ios::in | std::ios::binary);
 
     // 首先读入 StringSize
-    uint32_t inStringSize;
-    ifstream.read(reinterpret_cast<char *>(&inStringSize), sizeof(uint32_t));
+    std::streamsize inStringSize;
+    ifstream.read(reinterpret_cast<char *>(&inStringSize), sizeof(std::streamsize));
 
     // 创建一个 buffer 来读取
     std::vector<char> temp(inStringSize);
@@ -58,20 +72,20 @@ int main() {
     std::string inString(temp.begin(), temp.end());
 
     // 读取一维 Vector
-    uint32_t inVector1DSize;
-    ifstream.read(reinterpret_cast<char *>(&inVector1DSize), sizeof(uint32_t));
+    std::streamsize inVector1DSize;
+    ifstream.read(reinterpret_cast<char *>(&inVector1DSize), sizeof(std::streamsize));
     std::vector<double> inVector1D(inVector1DSize);
     ifstream.read(reinterpret_cast<char *>(&inVector1D[0]), std::streamsize(inVector1DSize * sizeof(double)));
 
     // 读取二维 Vector
-    uint32_t inVector2DSize;
-    ifstream.read(reinterpret_cast<char *>(&inVector2DSize), sizeof(uint32_t));
+    std::streamsize inVector2DSize;
+    ifstream.read(reinterpret_cast<char *>(&inVector2DSize), sizeof(std::streamsize));
 
     // 声明下
     std::vector<std::vector<double>> inVector2D;
-    for (uint32_t n = 0; n < inVector2DSize; ++n) {
+    for (std::streamsize n = 0; n < inVector2DSize; ++n) {
         // 获取内层大小
-        uint32_t inVector2DInnerSize = 0;
+        std::streamsize inVector2DInnerSize = 0;
         ifstream.read(reinterpret_cast<char *>(&inVector2DInnerSize), sizeof(inVector2DInnerSize));
 
         // resize vector
