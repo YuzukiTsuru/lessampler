12034e390b29c50e73717f31a2687bb68e8c82e4
YuzukiTsuru <740291272@qq.com>
2022-07-10 10:01:46 +0800

    Clear program name: lgpl requirements

--- src/AudioProcess/AduioProcess.cpp
+++ src/AudioProcess/AduioProcess.cpp
@@ -1,12 +1,12 @@
 
 /*
  * Copyright (c) 2022. YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
- * This program is free software: you can redistribute it and/or modify
+ * lessampler is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License v3.0 as published by
  *  the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * You should have received a copy of the GNU Lesser General Public License v3.0
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with lessampler. If not, see <http://www.gnu.org/licenses/>.
  */
 
 //
