f90ea16856156d1e9e712d8d5ea53590e318654d
YuzukiTsuru <740291272@qq.com>
2022-07-15 22:25:40 +0800

    add comment at check sum
    Former-commit-id: 788e766abec090565712f43c9d67ff6ce6319787

--- src/ConfigUnit/SHA1.h
+++ src/ConfigUnit/SHA1.h
@@ -1,12 +1,14 @@
 /*
-    sha1.hpp - source code of
-
+ * Copyright (c) 2022. YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
+ * lessampler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License v3.0 as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * You should have received a copy of the GNU Lesser General Public License v3.0
+ * along with lessampler. If not, see <http://www.gnu.org/licenses/>.
     ============
     SHA-1 in C++
     ============
-
-    100% Public Domain.
-
     Original C Code
         -- Steve Reid <steve@edmweb.com>
     Small changes to fit into bglibs
