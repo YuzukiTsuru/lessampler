d6e363de73aa70dea3e1ffa02c36cf48c9aafeaa
YuzukiTsuru <740291272@qq.com>
2022-07-23 10:43:21 +0800

    StaticCast: fix Windows Build issue

--- src/StaticCast.h
+++ src/StaticCast.h
@@ -17,21 +17,21 @@
 
 #include <cmath>
 
-#define _PI                      3.14159265358979               // PI
-#define _HALF_PI                 1.57079632679490               // PI / 2
-#define _QUARTER_PI              0.78539816339745               // PI / 4
-#define _TWAIN_PI                6.28318530717958               // PI * 2
-#define _E                       2.71828182845905               // e
-#define _LOG2E                   1.44269504088896               // log2(e)
-#define _LOG10E                  0.43429448190325               // log10(e)
-#define _LN2                     0.69314718055995               // ln(2)
-#define _LN10                    2.30258509299405               // ln(10)
-#define _SQRT_2                  1.41421356237310               // sqrt(2)
-#define _SQRT_1_2                0.70710678118655               // 1/sqrt(2)
-#define _SQRT_3                  1.73205077648163               // sqrt(3)
+#define LESS_PI                      3.14159265358979               // PI
+#define LESS_HALF_PI                 1.57079632679490               // PI / 2
+#define LESS_QUARTER_PI              0.78539816339745               // PI / 4
+#define LESS_TWAIN_PI                6.28318530717958               // PI * 2
+#define LESS_E                       2.71828182845905               // e
+#define LESS_LOG2E                   1.44269504088896               // log2(e)
+#define LESS_LOG10E                  0.43429448190325               // log10(e)
+#define LESS_LN2                     0.69314718055995               // ln(2)
+#define LESS_LN10                    2.30258509299405               // ln(10)
+#define LESS_SQRT_2                  1.41421356237310               // sqrt(2)
+#define LESS_SQRT_1_2                0.70710678118655               // 1/sqrt(2)
+#define LESS_SQRT_3                  1.73205077648163               // sqrt(3)
 
-#define CONFIGFILENAME           "lessampler.fex"
+#define CONFIGFILENAME               "lessampler.fex"
 
-#define DUMP_DATA                0
+#define DUMP_DATA                    0
 
 #endif //LESSAMPLER_STATICCAST_H
