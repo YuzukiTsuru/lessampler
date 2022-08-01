58f8b3240ca228fab51849e404485fdea31480df
YuzukiTsuru <740291272@qq.com>
2022-08-01 21:39:34 +0800

    FileIO: Using WavIO insted of Filexxx

--- src/FileIO/WavIO.cpp
+++ src/FileIO/WavIO.cpp
@@ -1,27 +1,48 @@
 /*
  * Copyright (c) 2022. YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
- * lessampler is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License v3.0 as published by
  *  the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * You should have received a copy of the GNU Lesser General Public License v3.0
- * along with lessampler. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 //
-// @Author Ghost Gloomy on 2020/1/26.
+// Created by gloom on 2022/8/1.
 //
 
-#include <iostream>
-#include "FileReadUnit.h"
-
 #include "Utils/LOG.h"
+#include "WavIO.h"
 
 #if (defined (__WIN32__) || defined (_WIN32)) && !defined (__MINGW32__)
 #pragma warning(disable : 4996)
 #endif
 
-int FileReadUnit::GetAudioLength(const char *filename) {
+extern "C" {
+#include <sndfile.h>
+}
+
+void WavIO::WriteWav(const std::filesystem::path &path, double *x, long long x_length, int fs) {
+    SNDFILE *sf;
+    SF_INFO info;
+    info.channels = 1;
+    info.samplerate = fs;
+    info.frames = x_length;
+    info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+
+    sf = sf_open(path.string().c_str(), SFM_WRITE, &info);
+    if (sf == nullptr) {
+        YALL_ERROR_ << "Failed to open the file.";
+        exit(-1);
+    }
+
+    sf_write_double(sf, x, x_length);
+
+    sf_close(sf);
+}
+
+int WavIO::GetAudioLength(const char *filename) {
     SNDFILE *sf;
     SF_INFO info;
     info.format = 0;
@@ -34,7 +55,7 @@ int FileReadUnit::GetAudioLength(const char *filename) {
     return static_cast<int>(len);
 }
 
-int FileReadUnit::WavRead(const char *FilePath, double *output) {
+int WavIO::WavRead(const char *FilePath, double *output) {
     SNDFILE *sf;
     SF_INFO info;
     info.format = 0;
@@ -76,4 +97,3 @@ int FileReadUnit::WavRead(const char *FilePath, double *output) {
     delete[] buf;
     return info.samplerate;
 }
-
--- src/FileIO/WavIO.h
+++ src/FileIO/WavIO.h
@@ -1,31 +1,27 @@
 /*
  * Copyright (c) 2022. YuzukiTsuru <GloomyGhost@GloomyGhost.com>.
- * lessampler is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License v3.0 as published by
  *  the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * You should have received a copy of the GNU Lesser General Public License v3.0
- * along with lessampler. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 //
-// @Author Ghost Gloomy on 2020/1/26.
+// Created by gloom on 2022/8/1.
 //
-#ifndef LESSAMPLER_FILEREADUNIT_H
-#define LESSAMPLER_FILEREADUNIT_H
 
-#include <iostream>
+#ifndef LESSAMPLER_WAVIO_H
+#define LESSAMPLER_WAVIO_H
+
 #include <filesystem>
+#include <iostream>
 
-
-extern "C" {
-#include <sndfile.h>
-}
-
-#define ENABLE_SNDFILE_WINDOWS_PROTOTYPES 1
-
-class FileReadUnit {
+class WavIO {
 public:
+    WavIO() = default;
+
     /*
      * Get Audio Length
      */
@@ -35,6 +31,10 @@ public:
      * Read Audio
      */
     static int WavRead(const char *FilePath, double *output);
+
+    static void WriteWav(const std::filesystem::path &path, double *x, long long x_length, int fs);
+
 };
 
-#endif //LESSAMPLER_FILEREADUNIT_H
+
+#endif //LESSAMPLER_WAVIO_H
